2645503315d8ace7ad1f758dd435e0e28ae6e044
Dilal Hassan <dilalhassan123@gmail.come>
2024-11-23 18:47:51 -0800

    color change

---
+++ index.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn');;
+
+//generate a random color
+const randomColor = () => {
+    let value = '0123456789ABCDEF';
+    let hash = '#';
+
+    for (let i = 0; i <= 5; i++) {
+        hash = hash + value[Math.floor(Math.random() * 16)];
+    }
+    return hash;
+}
+
+const changeColor = () => {
+    document.body.style.backgroundColor = randomColor();
+}
+
+btn.addEventListener('click', changeColor);
+
+// console.log('hellow ')
